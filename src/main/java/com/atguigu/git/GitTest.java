54b3efce608bd5bdae7e69c2ac5828eb0875704a
CjingXiao <1173460926@qq.com>
2024-06-03 19:23:56 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,16 @@
+package com.atguigu.git;
+
+/**
+ * ClassName: GitTest
+ * Package: com.atguigu.git
+ * Description:
+ *
+ * @Author cjxiao
+ * @Create 2024/6/3 19:17
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello git! ");
+    }
+}
